5dad32a8bbf44a784e8c2eb00dd895d72ab61125
Abisheak-Giltz <daabisheak@gmail.com>
2025-03-19 19:42:56 +0530

    Add files via upload
    User Input Store in Tuple

---
+++ User details store - Tuple.py
@@ -0,0 +1,10 @@
+#Tuple to store user input
+
+userName = input("Enter your name: ")
+userAge = input("Enter your age: ")
+userGender = input("Enter your gender: ")
+userCity = input("Enter your city: ")
+userJapaneseLevel = input("Enter your Japanese level: ")
+
+userDetails = (userName, userAge, userGender, userCity, userJapaneseLevel)
+print(userDetails)
