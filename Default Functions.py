a6877c9db4d149e05caf5d79cce37bd6671d234f
Abisheak <daabisheak@gmail.com>
2025-04-02 18:01:46 +0530

    Update Default Functions.py

--- Default Functions.py
+++ Default Functions.py
@@ -20,22 +20,30 @@ def dataAnalyzer(x, y, z) :
         aboveAverage += 1
     
     # The sum of all values
-    print(f"The Sum of the given Numbers is {x + y + z}")
+    total = x + y + z
     
     # The minimum of all values
-    print(f"The Minimum of the given Numbers is {min(numberList)}")
+    minimum = min(numberList)
 
     # The maximum of all values
-    print(f"The Maximum of the given Numbers is {max(numberList)}")
+    maximum = max(numberList)
 
     # The average of all values
-    print(f"The Average of the given Numbers is {average}")
+    #print(f"The Average of the given Numbers is {average}")
 
     # The number of values that are above average
-    print(f"The Number of values that is above average is {aboveAverage}")
+    #print(f"The Number of values that is above average is {aboveAverage}")
 
     # The sorted list of the values
-    print(f"The Sorted list is {sorted(numberList)}")
+    sort = sorted(numberList)
+
+    numericDict = {"sum" : total,
+                  "min" : minimum,
+                  "max" : maximum,
+                  "average" : average,
+                  "above average" : aboveAverage,
+                  "sorted" : sort}
+    print(numericDict)
 
 
 
