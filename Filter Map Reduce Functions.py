da84dba31a4333795c0007cf34baf0839af870f8
Abisheak <daabisheak@gmail.com>
2025-04-03 19:07:08 +0530

    Update Filter Map Reduce Functions.py

--- Filter Map Reduce Functions.py
+++ Filter Map Reduce Functions.py
@@ -1,12 +1,30 @@
 
-array = [-46, 5, 10, 15, -20, -8, 30]
+# Default functions Map(), Filter() & Reduce()
 
-def transformData(x) :
-    if (x < 0) :
-        return False
-    else:
-        return True
+def filterData(x) :
+    if(x >= 0) :
+        return x
 
 
-removeNegativeNumbers = filter(transformData, array)
-print(removeNegativeNumbers)
+
+myList = [12, -1, -4, 16, -5, -56, -13, 7, 61, 36, 5]
+filteredNegativeValues = list(filter(filterData, myList))
+print(filteredNegativeValues)
+
+
+def mapData(x) :
+    return x * 2
+
+
+squaredValues = list(map(mapData, myList))
+print(squaredValues)
+
+
+
+from functools import reduce
+
+def reduceProduct(x, y) :
+    return x + y
+
+reducedProduct = reduce(reduceProduct, squaredValues)
+print(reducedProduct)
